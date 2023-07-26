459e5f8896fe8956ba0b4ba34ecc4e30bdb39cf9
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-07-26 11:11:14 +0100

    Map docker container to run on port 3000. Make some changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Abdul's React App</h1>
+      <h1>Welcome to Abdul's Website!</h1>
+      <h4>This website is under construction! Please wait patiently.</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
