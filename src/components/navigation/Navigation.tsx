e8ec1116ae3a3fcb9b51c1c0ba6d39b4ba14ebf5
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-07-31 19:33:22 +0100

    Fix hidden nav item bug when resizing window from small to large. Add event listener to listen for window resize and set menu open to true if window size is more than 1024 px

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import IconButton from "./reusable/IconButton";
 import { XMarkIcon, Bars3Icon, MoonIcon, SunIcon, ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 
@@ -13,6 +13,20 @@ function Navigation({ isDarkMode, toggleDarkMode }: NavigationProp) {
   function toggleMenu() {
     setMenuOpen((prevState) => !prevState);
   }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setMenuOpen(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <nav className={`${isDarkMode ? "dark dark-nav": "light-nav"} w-full fixed top-0 z-50 flex items-center justify-between flex-wrap p-6`}>
@@ -35,24 +49,24 @@ function Navigation({ isDarkMode, toggleDarkMode }: NavigationProp) {
         <div className="text-base lg:flex lg:justify-center lg:flex-grow dark:text-white">
           <a 
             href="#About" 
-            className="hover:bg-grey-200 dark:hover:bg-black px-2 py-1 rounded block mt-4 lg:inline-block lg:mt-0 mr-4">
+            className="hover:bg-grey-200 dark:hover:bg-black px-3 py-2 rounded block mt-4 lg:inline-block lg:mt-0 mr-4">
             About
           </a>
           <a
             href="#Experience"
-            className="hover:bg-grey-200 dark:hover:bg-black px-2 py-1 rounded block mt-4 lg:inline-block lg:mt-0 mr-4"
+            className="hover:bg-grey-200 dark:hover:bg-black px-3 py-2 rounded block mt-4 lg:inline-block lg:mt-0 mr-4"
           >
             Experience
           </a>
           <a
             href="#Projects"
-            className="hover:bg-grey-200 dark:hover:bg-black px-2 py-1 rounded block mt-4 lg:inline-block lg:mt-0 mr-4"
+            className="hover:bg-grey-200 dark:hover:bg-black px-3 py-2 rounded block mt-4 lg:inline-block lg:mt-0 mr-4"
           >
             Projects
           </a>
           <a 
             href="#Contact" 
-            className="hover:bg-grey-200 dark:hover:bg-black px-2 py-1 rounded block mt-4 lg:inline-block lg:mt-0">
+            className="hover:bg-grey-200 dark:hover:bg-black px-3 py-2 rounded block mt-4 lg:inline-block lg:mt-0">
             Contact
           </a>
         </div>
