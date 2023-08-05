75db2097aea3ea3e6f291943913704f5040c15c8
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-08-05 15:06:27 +0100

    Add scale animation to headings on landing page

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 type LandingPageProp = {
   isDarkMode: boolean;
 };
@@ -5,11 +7,31 @@ type LandingPageProp = {
 function LandingPage({ isDarkMode }: LandingPageProp) {
   return (
     <div className={`${isDarkMode ? "dark" : ""} flex flex-col justify-center items-center h-screen`}>
-        <h1 className="font-bold">
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.4,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+          className="font-bold"
+        >
           Hi, I'm
           <span className="text-royal-blue-100"> Abdul!</span>
-        </h1>
-        <h4 className="mt-6">I'm a Software Engineer.</h4>
+        </motion.h1>
+        <motion.h4
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+          className="mt-6"
+        >
+          I'm a Software Engineer.
+        </motion.h4>
     </div>
   );
 }
