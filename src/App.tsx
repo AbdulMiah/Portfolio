4b1a318b9eff0d5f0efebbc25e03081fbc670089
AbdulMiah <51327213+AbdulMiah@users.noreply.github.com>
2023-08-18 00:00:39 +0100

    Merge pull request #10 from AbdulMiah/feature/2-landing-page
    Feature/2 landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import { useState, useEffect } from "react";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./components/pages/Landing/LandingPage";
 import Navigation from "./components/navigation/Navigation";
-
 
 function App() {
   const [isDarkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    const colourPreference = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    );
-  
+    const colourPreference = window.matchMedia("(prefers-color-scheme: dark)");
+
     if (colourPreference.matches) {
       setDarkMode(true);
     }
 
-    colourPreference.addEventListener("change", (evt) => setDarkMode(evt.matches));
+    colourPreference.addEventListener("change", (evt) =>
+      setDarkMode(evt.matches)
+    );
   }, []);
 
   function toggleDarkMode() {
     setDarkMode((prevState) => !prevState);
   }
   return (
-    <>
+    <div className="overflow-y-scroll w-full h-screen">
       <Navigation isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
       <LandingPage isDarkMode={isDarkMode} />
-    </>
+    </div>
   );
 }
 
---
+++ src/components/reusable/Avatar.tsx
@@ -0,0 +1,144 @@
+import * as THREE from "three";
+import { Group } from "three";
+import { useRef, useEffect } from "react";
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { useFrame } from "@react-three/fiber";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Wolf3D_Body: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Bottom: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Footwear: THREE.SkinnedMesh;
+    Wolf3D_Outfit_Top: THREE.SkinnedMesh;
+    Wolf3D_Hair: THREE.SkinnedMesh;
+    Wolf3D_Glasses: THREE.SkinnedMesh;
+    EyeLeft: THREE.SkinnedMesh;
+    EyeRight: THREE.SkinnedMesh;
+    Wolf3D_Head: THREE.SkinnedMesh;
+    Wolf3D_Teeth: THREE.SkinnedMesh;
+    Wolf3D_Beard: THREE.SkinnedMesh;
+    Hips: THREE.Bone;
+  };
+  materials: {
+    Wolf3D_Body: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Bottom: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Footwear: THREE.MeshStandardMaterial;
+    Wolf3D_Outfit_Top: THREE.MeshStandardMaterial;
+    Wolf3D_Hair: THREE.MeshStandardMaterial;
+    Wolf3D_Glasses: THREE.MeshStandardMaterial;
+    Wolf3D_Eye: THREE.MeshStandardMaterial;
+    Wolf3D_Skin: THREE.MeshStandardMaterial;
+    Wolf3D_Teeth: THREE.MeshStandardMaterial;
+    Wolf3D_Beard: THREE.MeshStandardMaterial;
+  };
+};
+
+type AvatarProp = {
+  lookAtPointer: boolean;
+} & JSX.IntrinsicElements["group"];
+
+export function Avatar(props: AvatarProp) {
+  const group = useRef<Group>(null);
+  const { nodes, materials } = useGLTF(
+    "src/assets/models/avatar.glb"
+  ) as GLTFResult;
+
+  const { animations: sittingAnimation } = useFBX(
+    "src/assets/animations/Sitting.fbx"
+  );
+  sittingAnimation[0].name = "Sitting";
+
+  const { actions } = useAnimations(sittingAnimation, group);
+
+  useFrame((state) => {
+    const pointer = new THREE.Vector3(state.mouse.x, state.mouse.y, 1);
+    if (props.lookAtPointer) {
+      group.current?.getObjectByName("Head")?.lookAt(pointer);
+    }
+  });
+
+  useEffect(() => {
+    actions["Sitting"]?.reset().play();
+  }, []);
+
+  return (
+    <group {...props} ref={group} dispose={null}>
+      <group rotation-x={-Math.PI / 2}>
+        <primitive object={nodes.Hips} />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Body.geometry}
+          material={materials.Wolf3D_Body}
+          skeleton={nodes.Wolf3D_Body.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+          material={materials.Wolf3D_Outfit_Bottom}
+          skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+          material={materials.Wolf3D_Outfit_Footwear}
+          skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Outfit_Top.geometry}
+          material={materials.Wolf3D_Outfit_Top}
+          skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Hair.geometry}
+          material={materials.Wolf3D_Hair}
+          skeleton={nodes.Wolf3D_Hair.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Wolf3D_Glasses.geometry}
+          material={materials.Wolf3D_Glasses}
+          skeleton={nodes.Wolf3D_Glasses.skeleton}
+        />
+        <skinnedMesh
+          name="EyeLeft"
+          geometry={nodes.EyeLeft.geometry}
+          material={materials.Wolf3D_Eye}
+          skeleton={nodes.EyeLeft.skeleton}
+          morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+          morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+        />
+        <skinnedMesh
+          name="EyeRight"
+          geometry={nodes.EyeRight.geometry}
+          material={materials.Wolf3D_Eye}
+          skeleton={nodes.EyeRight.skeleton}
+          morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+          morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+        />
+        <skinnedMesh
+          name="Wolf3D_Head"
+          geometry={nodes.Wolf3D_Head.geometry}
+          material={materials.Wolf3D_Skin}
+          skeleton={nodes.Wolf3D_Head.skeleton}
+          morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+          morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+        />
+        <skinnedMesh
+          name="Wolf3D_Teeth"
+          geometry={nodes.Wolf3D_Teeth.geometry}
+          material={materials.Wolf3D_Teeth}
+          skeleton={nodes.Wolf3D_Teeth.skeleton}
+          morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+          morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+        />
+        <skinnedMesh
+          name="Wolf3D_Beard"
+          geometry={nodes.Wolf3D_Beard.geometry}
+          material={materials.Wolf3D_Beard}
+          skeleton={nodes.Wolf3D_Beard.skeleton}
+          morphTargetDictionary={nodes.Wolf3D_Beard.morphTargetDictionary}
+          morphTargetInfluences={nodes.Wolf3D_Beard.morphTargetInfluences}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("src/assets/models/avatar.glb");
