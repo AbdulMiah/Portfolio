df19afcade5ed0778a70faf8ea7f6562082bf9f1
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-09-19 21:04:13 +0100

    Add scale animation to socials icons

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,21 +1,24 @@
 import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
 import { motion } from "framer-motion";
 import { socialLinks } from "../constants";
+import { scaleAnimation } from "../utils/motionVariants";
 
 function Socials() {
   return (
     <div className="flex space-x-6">
       <motion.a
-        whileHover={{ scale: [null, 1.3, 1.2] }}
-        transition={{ duration: 0.1 }}
+        variants={scaleAnimation([null, 1.3, 1.2], 0.1)}
+        initial="hidden"
+        whileHover="show"
         href={socialLinks.linkedin}
         target="_blank"
       >
         <IconBrandLinkedin className="hover:fill-royal-blue-200" />
       </motion.a>
       <motion.a
-        whileHover={{ scale: [null, 1.3, 1.2] }}
-        transition={{ duration: 0.1 }}
+        variants={scaleAnimation([null, 1.3, 1.2], 0.1)}
+        initial="hidden"
+        whileHover="show"
         href={socialLinks.github}
         target="_blank"
       >
