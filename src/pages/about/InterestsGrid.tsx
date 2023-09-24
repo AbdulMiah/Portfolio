8dca79cfcf1ebc9d102146138dd7b114f65b4ab0
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-09-24 20:41:18 +0100

    Add hover scale animation to cards. Change bg color if in dark mode

--- src/pages/about/InterestsGrid.tsx
+++ src/pages/about/InterestsGrid.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { scaleAnimation } from "../../utils/motionVariants";
+
 type InterestsGridProp = {
   isDarkMode: boolean;
   interests: any;
@@ -7,13 +10,18 @@ function InterestsGrid({ isDarkMode, interests }: InterestsGridProp) {
   return (
     <div className="grid grid-cols-3 grid-rows-2 gap-5">
       {interests.map((interest: any, index: number) => (
-        <div
+        <motion.div
+          variants={scaleAnimation([null, 1.2, 1.1], 0.1)}
+          initial="hidden"
+          whileHover="show"
           key={`interest-${index}`}
-          className="flex flex-col items-center justify-center p-5 bg-black"
+          className={`${
+            isDarkMode ? "bg-black" : "bg-grey-200"
+          } flex flex-col items-center justify-center rounded-xl p-5`}
         >
           <interest.icon className="h-8 w-8 mb-3" />
           <p className="text-lg uppercase">{interest.title}</p>
-        </div>
+        </motion.div>
       ))}
     </div>
   );
