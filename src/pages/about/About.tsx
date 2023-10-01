1ac410934b01aff8d71a42eba18d7e22af65ab9e
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-10-01 20:03:22 +0100

    Fix overlapping issue on mobile view for about section. Remove screen height

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -18,101 +18,101 @@ function About({ isDarkMode, isMobile }: AboutProp) {
   const [description, setDescription] = useState("");
 
   return (
-    <section className="relative w-full h-screen">
-      <div
-        className={`grid ${isMobile ? "grid-cols-1" : "grid-cols-3"} gap-7 p-5`}
-      >
-        {!isMobile && (
-          <div className="col-span-1 row-span-6 bg-royal-blue-100">
-            <div className="justify-center">
-              <h1>Avatar</h1>
-            </div>
+    <section
+      className={`grid ${
+        isMobile ? "grid-cols-1" : "grid-cols-3"
+      } gap-7 p-5 mb-10`}
+    >
+      {!isMobile && (
+        <div className="col-span-1 row-span-6 bg-royal-blue-100">
+          <div className="justify-center">
+            <h1>Avatar</h1>
           </div>
+        </div>
+      )}
+
+      <motion.div
+        variants={slideDown(0.2)}
+        className={`${isMobile ? "" : "col-span-2 col-start-2"} font-bold`}
+      >
+        {isMobile ? (
+          <h2>
+            About
+            <span className="text-royal-blue-100"> Me.</span>
+          </h2>
+        ) : (
+          <h1>
+            About
+            <span className="text-royal-blue-100"> Me.</span>
+          </h1>
         )}
+      </motion.div>
 
-        <motion.div
-          variants={slideDown(0.2)}
-          className={`${isMobile ? "" : "col-span-2 col-start-2"} font-bold`}
-        >
-          {isMobile ? (
-            <h2>
-              About
-              <span className="text-royal-blue-100"> Me.</span>
-            </h2>
-          ) : (
-            <h1>
-              About
-              <span className="text-royal-blue-100"> Me.</span>
-            </h1>
-          )}
-        </motion.div>
+      <motion.div
+        variants={popOutAnimation(0.8, 0.4)}
+        className={`${isMobile ? "" : "col-span-2 col-start-2"} text-lg`}
+      >
+        {about.description}
+      </motion.div>
 
-        <motion.div
-          variants={popOutAnimation(0.8, 0.4)}
-          className={`${isMobile ? "" : "col-span-2 col-start-2"} text-lg`}
-        >
-          {about.description}
-        </motion.div>
+      <motion.h3
+        variants={popOutAnimation(0.8, 0.6)}
+        className={`${isMobile ? "" : "col-span-2 col-start-2"} font-bold`}
+      >
+        My
+        <span className="text-royal-blue-100"> Interests.</span>
+      </motion.h3>
 
-        <motion.h3
-          variants={popOutAnimation(0.8, 0.6)}
-          className={`${isMobile ? "" : "col-span-2 col-start-2"} font-bold`}
-        >
-          My
-          <span className="text-royal-blue-100"> Interests.</span>
-        </motion.h3>
+      <motion.div
+        variants={popOutAnimation(0.8, 0.8)}
+        className={`${isMobile ? "" : "col-span-1 col-start-2"}`}
+      >
+        <InterestsGrid
+          isDarkMode={isDarkMode}
+          interests={about.interests}
+          setDescription={setDescription}
+        />
+      </motion.div>
 
-        <motion.div
-          variants={popOutAnimation(0.8, 0.8)}
-          className={`${isMobile ? "" : "col-span-1 col-start-2"}`}
-        >
-          <InterestsGrid
+      <motion.div
+        variants={popOutAnimation(0.8, 1)}
+        className={`${isMobile ? "" : "col-span-1 col-start-3"}`}
+      >
+        {description ? (
+          <InterestsPopup
             isDarkMode={isDarkMode}
-            interests={about.interests}
+            description={description}
             setDescription={setDescription}
           />
-        </motion.div>
+        ) : (
+          <div className="h-full w-full flex justify-center items-center animate-pulse space-x-2">
+            {isMobile ? (
+              <IconArrowUp className="h-7 w-7" />
+            ) : (
+              <IconArrowLeft className="h-7 w-7" />
+            )}
 
-        <motion.div
-          variants={popOutAnimation(0.8, 1)}
-          className={`${isMobile ? "" : "col-span-1 col-start-3"}`}
-        >
-          {description ? (
-            <InterestsPopup
-              isDarkMode={isDarkMode}
-              description={description}
-              setDescription={setDescription}
-            />
-          ) : (
-            <div className="h-full w-full flex justify-center items-center animate-pulse space-x-2">
-              {isMobile ? (
-                <IconArrowUp className="h-7 w-7" />
-              ) : (
-                <IconArrowLeft className="h-7 w-7" />
-              )}
-
-              <span> Click for more Info</span>
-            </div>
-          )}
-        </motion.div>
-
-        <motion.div
-          variants={popOutAnimation(0.8, 1.2)}
-          className={`${
-            isMobile ? "justify-between" : "col-span-2 col-start-2 space-x-48"
-          } flex items-center`}
-        >
-          <a href={cvPath} download={true}>
-            <IconButton icon={<IconDownload />} text="Download CV" />
-          </a>
-          <div>
-            Follow me:
-            <span>
-              <Socials />
-            </span>
+            <span> Click for more Info</span>
           </div>
-        </motion.div>
-      </div>
+        )}
+      </motion.div>
+
+      <motion.div
+        variants={popOutAnimation(0.8, 1.2)}
+        className={`${
+          isMobile ? "justify-between" : "col-span-2 col-start-2 space-x-48"
+        } flex items-center`}
+      >
+        <a href={cvPath} download={true}>
+          <IconButton icon={<IconDownload />} text="Download CV" />
+        </a>
+        <div>
+          Follow me:
+          <span>
+            <Socials />
+          </span>
+        </div>
+      </motion.div>
     </section>
   );
 }
