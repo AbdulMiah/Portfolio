975a2b899f67b769ca595be711f0a275d65a6b2a
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-12-19 21:23:52 +0000

    Style the project details page to include a grid of 3 cols that collapses to 1 in mobile view. Add back button to navigate to /project/all, and display project title, description and skills

--- src/pages/projects/ProjectDetails.tsx
+++ src/pages/projects/ProjectDetails.tsx
@@ -1,6 +1,8 @@
-import { motion } from "framer-motion";
-import { slideDown, staggerAnimation } from "../../utils/motionVariants";
-import { Project } from "../../utils/types";
+import { Project, Skill } from "../../utils/types";
+import { Link } from "react-router-dom";
+import SkillButton from "../../components/SkillButton";
+import { IconArrowLeft, IconBrandGithub } from "@tabler/icons-react";
+import IconButton from "../../components/IconButton";
 
 type ProjectDetailsProp = {
   isDarkMode: boolean;
@@ -10,29 +12,42 @@ type ProjectDetailsProp = {
 
 function ProjectDetails({ isMobile, isDarkMode, project }: ProjectDetailsProp) {
   return (
-    <motion.section
-      variants={staggerAnimation(0.2)}
-      initial="hidden"
-      whileInView="show"
-      className="relative w-full h-full overflow-hidden p-5 mt-[100px]"
+    <section
+      className={`grid ${
+        isMobile ? "grid-cols-1" : "grid-cols-[5%,80%,5%]"
+      } gap-7 p-5 mt-[100px]`}
     >
-      <motion.div
-        variants={slideDown(0.2)}
-        className="font-bold text-center mb-14"
-      >
-        {isMobile ? (
-          <>
-            <h3>{project.title}</h3>
-          </>
-        ) : (
-          <>
-            <h2>{project.title}</h2>
-          </>
-        )}
-
-        <h1>Project Details Page</h1>
-      </motion.div>
-    </motion.section>
+      <div className="flex flex-row mt-5 mb-5">
+        <Link
+          to="/project/all"
+          className="flex space-x-2"
+          onClick={() => {
+            window.scrollTo({ top: 0 });
+          }}
+        >
+          <IconArrowLeft className="bg-royal-blue-100 rounded-lg text-white" />
+          <span>Back</span>
+        </Link>
+      </div>
+      <div className="grid-rows-4 space-y-5">
+        <div className="flex flex-row justify-between items-center">
+          {isMobile ? <h3>{project.title}</h3> : <h2>{project.title}</h2>}
+          <IconButton icon={<IconBrandGithub />} text="Open Project" />
+        </div>
+        <div>{project.description}</div>
+        <div className="flex flex-wrap gap-1 justify-start">
+          {project.skills.map((skill: Skill, index: number) => (
+            <SkillButton
+              key={`skill-${index}`}
+              text={skill.title}
+              icon={<skill.icon />}
+              isDarkMode={isDarkMode}
+            />
+          ))}
+        </div>
+      </div>
+      <div>Next Project</div>
+    </section>
   );
 }
 
