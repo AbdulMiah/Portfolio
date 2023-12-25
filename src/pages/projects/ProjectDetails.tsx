2825bd7835cb0b9293cafaf5530ccbfe3a56f0e6
AbdulMiah <abdulmuktadirmiah@gmail.com>
2023-12-25 14:04:09 +0000

    Split middle column into 2 and add empty placeholder div for 3d model.

--- src/pages/projects/ProjectDetails.tsx
+++ src/pages/projects/ProjectDetails.tsx
@@ -14,7 +14,7 @@ function ProjectDetails({ isMobile, isDarkMode, project }: ProjectDetailsProp) {
   return (
     <section
       className={`grid ${
-        isMobile ? "grid-cols-1" : "grid-cols-[5%,80%,5%]"
+        isMobile ? "grid-cols-1" : "grid-cols-[10%,70%,10%]"
       } gap-7 p-5 mt-[100px]`}
     >
       <div className="flex flex-row">
@@ -29,35 +29,43 @@ function ProjectDetails({ isMobile, isDarkMode, project }: ProjectDetailsProp) {
           <span>Back</span>
         </Link>
       </div>
-      <div className="grid-rows-4 space-y-5">
-        <div
-          className={`flex ${
-            isMobile
-              ? "flex-col space-y-2"
-              : "flex-row justify-between items-center"
-          }`}
-        >
-          {isMobile ? <h3>{project.title}</h3> : <h2>{project.title}</h2>}
-          <a href={project.link} target="_blank">
-            <IconButton
-              icon={<IconBrandGithub />}
-              text="Open Project"
-              tooltipText="View Source Code on GitHub"
-            />
-          </a>
+
+      <div className={`grid ${isMobile ? "" : "grid-cols-[60%,40%]"} gap-5`}>
+        <div className="space-y-5">
+          <div
+            className={`flex ${
+              isMobile
+                ? "flex-col space-y-2"
+                : "flex-row justify-between items-center"
+            }`}
+          >
+            {isMobile ? <h3>{project.title}</h3> : <h2>{project.title}</h2>}
+            <a href={project.link} target="_blank">
+              <IconButton
+                icon={<IconBrandGithub />}
+                text="Open Project"
+                tooltipText="View Source Code on GitHub"
+              />
+            </a>
+          </div>
+          <div>{project.description}</div>
+          <div className="flex flex-wrap gap-1 justify-start">
+            {project.skills.map((skill: Skill, index: number) => (
+              <SkillButton
+                key={`skill-${index}`}
+                text={skill.title}
+                icon={<skill.icon />}
+                isDarkMode={isDarkMode}
+              />
+            ))}
+          </div>
         </div>
-        <div>{project.description}</div>
-        <div className="flex flex-wrap gap-1 justify-start">
-          {project.skills.map((skill: Skill, index: number) => (
-            <SkillButton
-              key={`skill-${index}`}
-              text={skill.title}
-              icon={<skill.icon />}
-              isDarkMode={isDarkMode}
-            />
-          ))}
-        </div>
-        <div className="flex flex-col items-center space-y-5">
+
+        {!isMobile && (
+          <div className="flex items-center justify-center">3D model</div>
+        )}
+
+        <div className="flex flex-col col-span-2 items-center space-y-5">
           {project.images.map((image, index) => (
             <img
               src={image}
@@ -70,6 +78,7 @@ function ProjectDetails({ isMobile, isDarkMode, project }: ProjectDetailsProp) {
           ))}
         </div>
       </div>
+
       <div>Next Project</div>
     </section>
   );
