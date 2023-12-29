d97221bbbc6e5baebf07c8de711c85b4a4d3dbbb
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-12-29 20:17:38 +0000

    Use emailJs to send email when form is submitted

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -6,15 +6,17 @@ type IconButtonProp = {
   icon: JSX.Element;
   text?: string;
   tooltipText?: string;
+  type?: "submit" | "reset" | "button" | undefined;
 };
 
-function IconButton({ icon, text, tooltipText }: IconButtonProp) {
+function IconButton({ icon, text, tooltipText, type }: IconButtonProp) {
   return (
     <Tooltip title={tooltipText}>
       <motion.button
         variants={scaleAnimation([null, 1.2, 1.1], 0.1)}
         initial="hidden"
         whileHover="show"
+        type={type}
         className="bg-royal-blue-100 hover:bg-royal-blue-200 text-white py-2 px-3 rounded-[12px] inline-flex space-x-4"
       >
         {text && <span className="text-base">{text}</span>}
--- src/pages/contact/Form.tsx
+++ src/pages/contact/Form.tsx
@@ -2,21 +2,35 @@ import { IconSend } from "@tabler/icons-react";
 import IconButton from "../../components/IconButton";
 import { motion } from "framer-motion";
 import { slideDown } from "../../utils/motionVariants";
+import emailjs from "@emailjs/browser";
 
 type FormProp = {
   isDarkMode: boolean;
 };
 
 function Form({ isDarkMode }: FormProp) {
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_dggxdwd",
+      "template_j5fdmsn",
+      e.target,
+      "1WMou0YgjyHfq-I6j"
+    );
+  };
+
   return (
     <motion.form
       variants={slideDown(0.6)}
       className="flex flex-col gap-5 w-1/2"
+      onSubmit={sendEmail}
     >
       <label className="flex flex-col">
         <span className="self-start">Your Name</span>
         <input
           type="text"
+          name="from_name"
           className={`${
             isDarkMode ? "bg-dark-300" : "bg-grey-200"
           } rounded-xl p-2`}
@@ -27,6 +41,7 @@ function Form({ isDarkMode }: FormProp) {
         <span className="self-start">Your Email</span>
         <input
           type="email"
+          name="from_email"
           className={`${
             isDarkMode ? "bg-dark-300" : "bg-grey-200"
           } rounded-xl p-2`}
@@ -36,6 +51,7 @@ function Form({ isDarkMode }: FormProp) {
       <label className="flex flex-col">
         <span className="self-start">Your Message</span>
         <textarea
+          name="message"
           className={`${
             isDarkMode ? "bg-dark-300" : "bg-grey-200"
           } rounded-xl p-2`}
@@ -43,7 +59,7 @@ function Form({ isDarkMode }: FormProp) {
       </label>
 
       <div>
-        <IconButton text="Send Message" icon={<IconSend />} />
+        <IconButton text="Send Message" icon={<IconSend />} type="submit" />
       </div>
     </motion.form>
   );
