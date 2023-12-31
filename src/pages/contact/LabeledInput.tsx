20cd0ddb92f244a9801c93c274f28d6ffcb07e60
Abdul Miah <abdulmuktadirmiah@gmail.com>
2023-12-31 13:51:35 +0000

    remove unused framer animation const

--- src/pages/contact/LabeledInput.tsx
+++ src/pages/contact/LabeledInput.tsx
@@ -73,11 +73,4 @@ function LabeledInput({
   );
 }
 
-const framer_error = {
-  initial: { opacity: 0, y: 10 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 10 },
-  transition: { duration: 0.2 },
-};
-
 export default LabeledInput;
